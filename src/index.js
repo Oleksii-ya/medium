0af51a7cba25dc037b06c5f4efe462f584586a1d
Oleksii Kuryliuk <oleksii.kuryliuk@o-kuryliuk.local>
2022-05-10 13:49:32 +0300

    added logic to TopBar

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from 'routes'
 import TopBar from 'components/TopBar'
+import { CurrentUserProvider } from 'contexts/currentUser'
+import CurrentUserChecker from 'components/currentUserChecker'
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <TopBar/>
-        <Routes />
-      </Router>
-    </>
+    <CurrentUserProvider>
+      <CurrentUserChecker>
+        <Router>
+          <TopBar />
+          <Routes />
+        </Router>
+      </CurrentUserChecker>
+    </CurrentUserProvider>
   )
 }
 
